108194b2c2c25fb7669d24bbf7cf0e1187461346
njse22 <njse22@gmail.com>
2023-08-04 10:17:46 -0500

    Semana 1

--- Semana1/Recursion/src/model/Recursion.java
+++ Semana1/Recursion/src/model/Recursion.java
@@ -1,8 +1,16 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Recursion {
 
+    private ArrayList<Integer> dp;
+
     public Recursion() {
+        dp = new ArrayList<>(100);
+        for(int i = 0; i< 100; i++){
+            dp.add(null);
+        }
     }
 
     public long factorial(long n) {
@@ -16,6 +24,27 @@ public class Recursion {
         // caso recursivo
         else {
             result = n * factorial(n - 1);
+        }
+        return result;
+    }
+
+
+    public int factorialDP(int n) {
+        int result;
+
+        if(dp.get(n) == null){
+            // Caso base
+            if (n == 0) {
+                result = 1;
+            }
+
+            // caso recursivo
+            else {
+                result = n * factorialDP(n - 1);
+                dp.set(n, result);
+            }
+        }else {
+            result = dp.get(n);
         }
         return result;
     }
@@ -37,4 +66,33 @@ public class Recursion {
     }
 
 
+    // Método recursivo en dos partes
+    // Método fachada
+    public int sum(int[] arr){
+        return sum( arr, 1, arr[0]);
+    }
+
+    // Método que hace la recursión
+    // Método recursivo Iterativo: porque en cada "iteración" se modifican los parámetros dentro del método
+    private int sum(int[] arr, int index, int result){
+        // Caso base
+        if(index == arr.length){
+            return result;
+        }
+        // Caso recursivo
+        else {
+            result += arr[index];
+            return sum(arr, ++index , result);
+        }
+    }
+
+    
+
+    private int sum2(int[] arr, int index, int result){
+        return (index < arr.length) ? sum2(arr, index + 1, result+arr[index]) : result;
+    }
+
+    public ArrayList<Integer> getDp() {
+        return dp;
+    }
 }
--- Semana1/Recursion/src/ui/Main.java
+++ Semana1/Recursion/src/ui/Main.java
@@ -6,7 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Recursion r = new Recursion();
 
+        System.out.println("Ejemplo Factorial");
+        System.out.println(r.factorial(1));
+        System.out.println(r.factorial(2));
+        System.out.println(r.factorial(3));
+        System.out.println(r.factorial(4));
         System.out.println(r.factorial(5));
+
+        System.out.println("Ejemplo Factorial DP");
+        System.out.println(r.factorialDP(1));
+        System.out.println(r.factorialDP(2));
+        System.out.println(r.factorialDP(3));
+        System.out.println(r.factorialDP(4));
+        System.out.println(r.factorialDP(5));
+
+        System.out.println("Ejemplo Suma de elementos de un Array");
+        int[] arr = {12, -1, 15, 2, 4 , 14};
+        System.out.println(r.sum(arr));
+
+
+        System.out.println("Ejemplo Fibonacci");
         System.out.println(r.fibonnaci(4));
     }
 }
