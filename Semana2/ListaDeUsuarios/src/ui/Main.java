d95b20ddb1d1949af39d5880cee6e8eb146bb55b
njse22 <njse22@gmail.com>
2023-08-11 09:06:26 -0500

    Semana 2

--- Semana2/ListaDeUsuarios/src/ui/Main.java
+++ Semana2/ListaDeUsuarios/src/ui/Main.java
@@ -3,28 +3,102 @@ package ui;
 import model.User;
 import model.UserController;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
+    private UserController controller;
+    private Scanner sc;
+
+    public Main(){
+        controller = new UserController();
+        sc = new Scanner(System.in);
+        initUsers();
+    }
+
     public static void main(String[] args) {
-        UserController controller = new UserController();
+        Main m = new Main();
+        // variable para leer la entrada
+        int option= 0;
 
-        System.out.println("***************************");
-        System.out.println("Agregar Usuarios");
+        //ciclo para ejecutar el menu mientras que el usuario no
+        // elija la opciOn para salir (0)
+        do {
+            option = m.showMenuAndGetOption();
+            m.answerOption(option);
+        }while (option !=0);
+    }
 
-        User user1 = new User(0);
-        controller.addFirstUser(user1);
-        System.out.println(controller.getHead());
+    /**
+     * Metodo que se encarga de llamar a los mEtodos que resuelven cada uno de los
+     * requerimientos de la aplicaciOn
+     * @param userOption, int es el nUmero ingresado por el usuario (no ha sido validado)
+     */
+    public void answerOption(int userOption) {
+        switch (userOption) {
+            case 0:
+                System.out.println("cerrando la aplicaciOn, bye");
+                break;
+            case 1:
+                addUser();
+                break;
+            case 2:
+                searchUser();
+                break;
+            case 3:
+                deleteUser();
+                break;
+            case 4:
+                printUsers();
+                break;
+            case 5:
 
-        User user2 = new User(1);
-        controller.addFirstUser(user2);
-        System.out.println(controller.getHead());
+        }
+    }
+    private void deleteUser() {
 
-        User user3 = new User(2);
-        controller.addFirstUser(user3);
-        System.out.println(controller.getHead());
+    }
 
-        System.out.println("***************************");
-        System.out.println("Imprimir Lista de Usuarios");
+    private void searchUser() {
+
+    }
+
+    private void addUser() {
+
+    }
+
+    private void initUsers(){
+        for (int i = 0; i< 10; i++){
+            controller.addLastUser(new User(i));
+        }
+    }
+
+    private void printUsers(){
         System.out.println(controller.print());
     }
+
+    /**
+     * Metodo que muestra el menu de la aplicaciOn, a este metod hace falta implementar la validaciOn
+     * del valor ingresado por el usuario
+     * @return input, int es la opciOn elegida por el usuario
+     */
+    public int showMenuAndGetOption() {
+        int input;
+        System.out.println("\n\nMenu de la aplicación, digite una opciOn\n"+
+                "(1) agregar User\n" +
+                "(2) buscar User\n" +
+                "(3) eliminar User\n"+
+                "(4) opciOn 4\n"+
+                "(5) opciOn 5\n"+
+                "(0) Para salir"
+
+        );
+        input = sc.nextInt();
+        sc.nextLine();
+        return input;
+    }
+
+
 }
