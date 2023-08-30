041df0e451b649f5f24594ddba4fb133125d3f19
njse22 <njse22@gmail.com>
2023-08-30 08:33:25 -0500

    semana 5

--- Semana5/BST/src/model/BST.java
+++ Semana5/BST/src/model/BST.java
@@ -55,16 +55,80 @@ public class BST {
         }
     }
 
-    public String getMin(){
+    public Node getMin(){
         return getMin(root);
     }
 
-    private String getMin(Node current){
+    private Node getMin(Node current){
         if(current.getLeft() == null){
-            return current.getKey();
+            return current;
         }
         else {
             return getMin(current.getLeft());
         }
     }
+
+    public void delete(String goal){
+        delete(null, root, goal);
+    }
+
+    private void delete(Node parent, Node current, String goal){
+        if(current == null){
+            // el nodo no esta en el árbol
+        }
+        // Cuando encontramos el nodo
+        if(goal.equals(current.getKey())){
+            // El nodo es una hoja del árbol
+            if(current.getLeft() == null && current.getRight() == null){
+                if(current == this.root){
+                    this.root = null;
+                }
+                // evaluar si el nodo es hijo izquierdo
+                else if(parent.getLeft() == current){
+                    parent.setLeft(null);
+                }
+                // evaluar si el nodo es hijo derecho
+                else {
+                    parent.setRight(null);
+                }
+            }
+            // el nodo a eliminar tiene unicamente un hijo izquierdo
+            else if (current.getLeft() != null && current.getRight() == null) {
+                if(parent.getLeft() == current){
+                    parent.setLeft(current.getLeft());
+                } else {
+                    parent.setRight(current.getLeft());
+                }
+            }
+            // el nodo a eliminar tiene unicamente un hijo derecho
+            else if (current.getLeft() == null && current.getRight() != null) {
+                // el nodo es un hijo izquierdo
+                if(parent.getLeft() == current){
+                    parent.setLeft(current.getRight());
+                }
+                // el nodo es un hijo derecho
+                else {
+                    parent.setRight(current.getRight());
+                }
+            } else if (current.getLeft() != null && current.getRight() != null ) {
+                // obtener el successor del nodo --> el menor nodo de los mayores
+                Node successor = getMin(current.getRight());
+
+                // un remplazo de los atributos
+                current.setKey(successor.getKey());
+                // current.setValue(successor.getValue());
+
+                // eliminar el successor --> actualizar las conexiones
+                delete(current, current.getRight(), successor.getKey());
+            }
+        }
+        // cuando no lo encuentro
+        else if (goal.compareTo(current.getKey()) < 0) {
+           delete(current, current.getLeft(), goal);
+        }
+        else if(goal.compareTo(current.getKey()) > 0){
+            delete(current, current.getRight(), goal);
+        }
+    }
+
 }
